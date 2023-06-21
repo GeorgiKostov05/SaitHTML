0351541fc57bf7a9db0dcc0bbd585f91cae85164
GeorgiKostov05 <120709555+GeorgiKostov05@users.noreply.github.com>
2023-06-21 13:13:11 +0300

    Add files via upload

---
+++ zadachaZaOcenka/PermanentTeacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachaZaOcenka
+{
+    public class PermanentTeacher:TeacherBase
+    {
+        public PermanentTeacher(string name, string school, List<string> classes, string subject)
+        {
+            Name = name;
+            School = school;
+            Classes = classes;
+            Subject = subject;
+            Contract = ContractType.Permanent;
+        }
+
+        public override bool IsTeachingSubject(string subject)
+        {
+            return Subject.Equals(subject, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ zadachaZaOcenka/TeacherBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachaZaOcenka
+{
+    public abstract class TeacherBase : ITeacher
+    {
+        public string Name { get; set; }
+        public string School { get; set; }
+        public List<string> Classes { get; set; }
+        public string Subject { get; set; }
+        public ContractType Contract { get; set; }
+
+        public abstract bool IsTeachingSubject(string subject);
+    }
+}
